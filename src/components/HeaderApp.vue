c25001eac9fde9174f19684cd839dc378187d64d
Jac22 <jacopo.prudentino@gmail.com>
2022-12-22 14:35:45 +0100

    Creato componente header

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,48 @@
+<script>
+import NavBar from './NavBar.vue'
+export default {
+    components: {
+        NavBar,
+    },
+    data() {
+        return {
+            logo: 'MasterStudy-1.svg',
+        }
+    },
+    methods: {
+        getImagePath: function (imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        },
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <img :src="getImagePath('../assets/images/' + logo)" alt="Logo MasterStudy">
+        <nav>
+            <NavBar />
+
+        </nav>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    max-width: 1170px;
+    height: 145px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img {
+        width: 250px;
+    }
+
+    nav {
+        width: calc(100% - 465px);
+        display: flex;
+        justify-content: space-between;
+    }
+}
+</style>
