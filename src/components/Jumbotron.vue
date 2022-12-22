0aaeb56b37c0e586659a074737174dca6ad6c963
Jac22 <jacopo.prudentino@gmail.com>
2022-12-22 15:54:35 +0100

    Creato componente Jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    data() {
+        return {
+            jumbo: 'slide-1.jpg'
+        }
+    },
+    methods: {
+        getImagePath: function (imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        },
+    }
+}
+</script>
+
+<template>
+    <div id="container-jumbo" :style="'background-image: url(' + getImagePath('../assets/images/' + this.jumbo) + ')'">
+        <div id="text-container">
+            <h1>
+                Start your Human <span>Revolution</span> today
+            </h1>
+            <p>
+                A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I
+                enjoy with my whole heart.
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables.scss' as *;
+
+#container-jumbo {
+    height: 100vh;
+    min-height: 700px;
+    max-height: 900px;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+
+    #text-container {
+        width: 600px;
+        position: absolute;
+        top: 220px;
+        left: calc(50% - 50px);
+
+        h1 {
+            font-size: 4.5rem;
+            line-height: 67px;
+            margin-bottom: 30px;
+
+            span {
+                color: $buttecup;
+            }
+        }
+
+        p {
+            color: $cape-cod;
+            font-size: 1.25rem;
+            width: 560px;
+            line-height: 30px;
+        }
+    }
+}
+</style>
